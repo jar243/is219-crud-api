5747820a34ea58bd76c7b2d5b524c892c45a7c81
fake name <bruh@fam.com>
2021-05-06 22:02:46 -0400

    created login, register, and protected pages

--- src/server.js
+++ src/server.js
@@ -1,22 +1,33 @@
 const express = require("express")
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars")
 
 const PORT = process.env.SERVER_PORT || 8080
 
 const app = express()
 
-app.engine('hbs', exphbs({
-  extname: '.hbs'
-}));
+app.engine(
+  "hbs",
+  exphbs({
+    extname: ".hbs",
+  })
+)
 
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs")
 
 const treeRouter = require("./tree.router")
 app.use("/api/trees", treeRouter)
 
-app.get('/', function (req, res) {
-  res.render('home');
-});
+app.get("/", function (req, res) {
+  res.render("home")
+})
+
+app.get("/register", (req, res) => {
+  res.render("register")
+})
+
+app.get("/login", (req, res) => {
+  res.render("login")
+})
 
 app.listen(PORT, function () {
   console.log("Node server started on %s ...", PORT)
