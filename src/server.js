e9a6650c81def7df6dccd4769bfb24e0531ac21f
fake name <bruh@fam.com>
2021-05-06 22:27:38 -0400

    added server-side registration code

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
 const express = require("express")
 const exphbs = require("express-handlebars")
+const cookieParser = require("cookie-parser")
+const bodyParser = require("body-parser")
+const crypto = require('crypto')
+
+const getHashedPassword = (password) => {
+  const sha256 = crypto.createHash('sha256');
+  const hash = sha256.update(password).digest('base64');
+  return hash;
+}
+
 
 const PORT = process.env.SERVER_PORT || 8080
 
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(cookieParser())
 
 app.engine(
   "hbs",
@@ -14,8 +28,12 @@ app.engine(
 
 app.set("view engine", "hbs")
 
+// BACKEND API
+
 const treeRouter = require("./tree.router")
 app.use("/api/trees", treeRouter)
+
+// FRONTEND VIEWS
 
 app.get("/", function (req, res) {
   res.render("home")
@@ -23,6 +41,46 @@ app.get("/", function (req, res) {
 
 app.get("/register", (req, res) => {
   res.render("register")
+})
+
+const users = [
+  {
+    username: "johndoe",
+    // This is the SHA256 hash for value of `password`
+    password: "XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=",
+  },
+]
+
+app.post("/register", (req, res) => {
+  const { username, password, confirmPassword } = req.body
+
+  if (password !== confirmPassword) {
+    res.render("register", {
+      message: "Password does not match.",
+      messageClass: "alert-danger",
+    })
+    return
+  }
+
+  if (users.find((user) => user.username === username)) {
+    res.render("register", {
+      message: "Username already in use.",
+      messageClass: "alert-danger",
+    })
+    return
+  }
+
+  const hashedPassword = getHashedPassword(password)
+
+  users.push({
+    username,
+    password: hashedPassword,
+  })
+
+  res.render("login", {
+    message: "Registration Complete. Please login to continue.",
+    messageClass: "alert-success",
+  })
 })
 
 app.get("/login", (req, res) => {
