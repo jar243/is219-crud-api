09b66a6e2133ab4129b2a3234ff791e9a1bcc893
fake name <bruh@fam.com>
2021-05-06 22:33:45 -0400

    added backend login code

--- src/server.js
+++ src/server.js
@@ -2,14 +2,30 @@ const express = require("express")
 const exphbs = require("express-handlebars")
 const cookieParser = require("cookie-parser")
 const bodyParser = require("body-parser")
-const crypto = require('crypto')
+const crypto = require("crypto")
+
+// AUTH-RELATED FUNCS AND VARS
 
 const getHashedPassword = (password) => {
-  const sha256 = crypto.createHash('sha256');
-  const hash = sha256.update(password).digest('base64');
-  return hash;
+  const sha256 = crypto.createHash("sha256")
+  const hash = sha256.update(password).digest("base64")
+  return hash
 }
 
+const generateAuthToken = () => {
+  return crypto.randomBytes(30).toString("hex")
+}
+
+const authTokens = {}
+const users = [
+  {
+    username: "johndoe",
+    // This is the SHA256 hash for value of `password`
+    password: "XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=",
+  },
+]
+
+// EXPRESS SETUP
 
 const PORT = process.env.SERVER_PORT || 8080
 
@@ -33,23 +49,17 @@ app.set("view engine", "hbs")
 const treeRouter = require("./tree.router")
 app.use("/api/trees", treeRouter)
 
-// FRONTEND VIEWS
+// HOME PAGE
 
 app.get("/", function (req, res) {
   res.render("home")
 })
 
+// REGISTRATION CODE
+
 app.get("/register", (req, res) => {
   res.render("register")
 })
-
-const users = [
-  {
-    username: "johndoe",
-    // This is the SHA256 hash for value of `password`
-    password: "XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=",
-  },
-]
 
 app.post("/register", (req, res) => {
   const { username, password, confirmPassword } = req.body
@@ -83,9 +93,35 @@ app.post("/register", (req, res) => {
   })
 })
 
+// LOGIN CODE
+
 app.get("/login", (req, res) => {
   res.render("login")
 })
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body
+  const hashedPassword = getHashedPassword(password)
+
+  const user = users.find((u) => {
+    return u.username === username && hashedPassword === u.password
+  })
+
+  if (!user) {
+    res.render("login", {
+      message: "Invalid username or password",
+      messageClass: "alert-danger",
+    })
+    return
+  }
+
+  const authToken = generateAuthToken()
+  authTokens[authToken] = user
+  res.cookie("AuthToken", authToken)
+  res.redirect("/protected")
+})
+
+// START SERVER
 
 app.listen(PORT, function () {
   console.log("Node server started on %s ...", PORT)
