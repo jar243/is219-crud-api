9e84e1117154892019d337188ca031bf65007c78
fake name <bruh@fam.com>
2021-05-06 21:54:19 -0400

    setup basic handlebars homepage

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
-const PORT = process.env.SERVER_PORT || 8080
-
 const express = require("express")
+const exphbs = require('express-handlebars');
+
+const PORT = process.env.SERVER_PORT || 8080
 
 const app = express()
 
-app.use(express.static("docs"))
+app.engine('hbs', exphbs({
+  extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
 
 const treeRouter = require("./tree.router")
 app.use("/api/trees", treeRouter)
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
 
 app.listen(PORT, function () {
   console.log("Node server started on %s ...", PORT)
