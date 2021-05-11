f6b8cd4290411725c265b2844f17c2e110ff8434
fake name <bruh@fam.com>
2021-05-10 21:51:18 -0400

    working session w passport and auth0

--- src/server.js
+++ src/server.js
@@ -3,42 +3,54 @@ const cors = require("cors")
 const exphbs = require("express-handlebars")
 const cookieParser = require("cookie-parser")
 const bodyParser = require("body-parser")
+const expressSession = require("express-session")
+const passport = require("passport")
+const Auth0Strategy = require("passport-auth0")
 
-// MIDDLEWARE
-
-const requireAuth = (req, res, next) => {
-  if (req.user) {
-    next()
-  } else {
-    res.send("You must be authenticated to perform this action")
-  }
-}
-
-// CONSTANTS
-
-const accessTokens = {}
-const users = [
-  {
-    username: "johnrezk",
-    // This is the SHA256 hash for value of `password`
-    password: "XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=",
-  },
-]
+require("dotenv").config()
 
 // EXPRESS SETUP
 
+const app = express()
 const PORT = process.env.SERVER_PORT || 8080
 
-const app = express()
+// SESSION CONFIG
+
+const session = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: false,
+}
+
+// PASSPORT CONFIG
+
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL: process.env.AUTH0_CALLBACK_URL,
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    /**
+     * Access tokens are used to authorize users to an API
+     * (resource server)
+     * accessToken is the token to call the Auth0 API
+     * or a secured third-party API
+     * extraParams.id_token has the JSON Web Token
+     * profile has all the information from the user
+     */
+    return done(null, profile)
+  }
+)
+
+// MIDDLEWARE
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use(cookieParser())
-app.use((req, res, next) => {
-  const accessToken = req.cookies["AccessToken"]
-  req.user = accessTokens[accessToken]
-  next()
-})
+
+// APP SETUP
 
 app.engine(
   "hbs",
@@ -46,16 +58,27 @@ app.engine(
     extname: ".hbs",
   })
 )
-
 app.set("view engine", "hbs")
+
+app.use(expressSession(session))
+
+passport.use(strategy)
+app.use(passport.initialize())
+app.use(passport.session())
+passport.serializeUser((user, done) => {
+  done(null, user)
+})
+passport.deserializeUser((user, done) => {
+  done(null, user)
+})
 
 // API ROUTERS
 
 const treeRouter = require("./tree.router")
-app.use("/api/trees", requireAuth, treeRouter)
+app.use("/api/trees", treeRouter)
 
-const authRouter = require("./auth")(users, accessTokens)
-app.use("/api/auth", authRouter)
+// const authRouter = require("./auth")(users, accessTokens)
+// app.use("/api/auth", authRouter)
 
 // HOME PAGE
 
@@ -63,21 +86,40 @@ app.get("/", function (req, res) {
   res.render("home")
 })
 
-// REGISTRATION CODE
-
-app.get("/register", (req, res) => {
-  res.render("register")
-})
-
 // LOGIN CODE
 
-app.get("/login", (req, res) => {
-  res.render("login")
+app.get(
+  "/login",
+  passport.authenticate("auth0", {
+    scope: "openid email profile",
+  }),
+  (req, res) => {
+    res.redirect("/")
+  }
+)
+
+app.get("/callback", (req, res, next) => {
+  passport.authenticate("auth0", (err, user, info) => {
+    if (err) {
+      return next(err)
+    }
+    if (!user) {
+      return res.redirect("/login")
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err)
+      }
+      const returnTo = req.session.returnTo
+      delete req.session.returnTo
+      res.redirect(returnTo || "/")
+    })
+  })(req, res, next)
 })
 
 // PROTECTED PAGE WITH MIDDLEWARE
 
-app.get("/dashboard", requireAuth, (req, res) => {
+app.get("/dashboard", (req, res) => {
   res.render("dashboard")
 })
 
